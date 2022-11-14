c25f2da4d4ae8082a7588bbf41eb1b1d5b94b225
vijay.kumar@iorbit-tech.com <vijay.kumar@iorbit-tech.com>
2022-11-14 18:59:04 +0530

    Login Page initial code

--- src/Components/LoginForm/Index.jsx
+++ src/Components/LoginForm/Index.jsx
@@ -1,8 +1,69 @@
-import { Paper } from "@mui/material";
 import React from "react";
+import { Paper } from "@mui/material";
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const LoginForm = () => {
-  return <Paper elevation={3} style={{ height: 450, width: 420 }}></Paper>;
+  const [values, setValues] = React.useState({
+    password: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <Paper elevation={3} style={{ height: 450, width: 420 }}>
+      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+        <TextField
+          required
+          id="outlined-required"
+          label="UserName"
+          defaultValue="UserName"
+          margin="normal"
+        />
+      </FormControl>
+      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.password}
+          onChange={handleChange('password')}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {values.showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
+    </Paper>
+  )
 };
 
 export default LoginForm;
